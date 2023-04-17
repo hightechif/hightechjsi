620fd3ba4f76c5a7a11f4da6dee5012b0b56354e
R. Fadhil <ridhan.fadhilah@sg-dsa.com>
2023-04-17 13:50:37 +0700

    FEAT: Android Web View JS Interface

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@ import './App.css';
 import Button from './component/Button';
 
 function App() {
+
+  const accessPermission = () => {
+    if (window.jsi !== undefined) {
+      const isGranted = window.jsi?.doAction("location")
+      alert(`isGranted=${isGranted}`)
+      return isGranted
+    }
+  }
+
+  const getLatLong = () => {
+    if (window.jsi !== undefined) {
+      const data = window.jsi?.doAction("location")
+      alert(`data=${data}`)
+      return data
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <Button onClick={() => alert("Muncul")} label="Permission" />
-        <Button onClick={() => alert("Muncul")} label="Lat Long" />
+        <Button onClick={accessPermission} label="Permission" />
+        <Button onClick={getLatLong} label="Lat Long" />
       </header>
     </div>
   );
