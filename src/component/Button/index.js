baecd516eb1eeb0639bd8ab6fd15f5cb565c3625
R. Fadhil <ridhan.fadhilah@sg-dsa.com>
2023-04-17 13:33:42 +0700

    FEAT: First UI Setup

---
+++ src/component/Button/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = (props) => {
+    
+    const {onClick, label } =  props
+
+    return (
+        <button onClick={onClick}>{label}</button>
+    )
+}
+
+export default Button
